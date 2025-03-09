090c305d148f161a7fa0834ea26e8aa507edf0d2
Ankit Raj <ankitrajkumar7667162310@gmail.com>
2025-03-09 14:44:03 +0530

    [Ankit Raj] Added Code for UC2

--- EmployeeWageSystem.js
+++ EmployeeWageSystem.js
@@ -1,11 +1,24 @@
-///////////// UC1 ///////////////////
-const IS_ABSENT=0;
+//////////// UC2 /////////////////////
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
 
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == IS_ABSENT) {
-    console.log("Employee is Absent");
-    return;
+let empHours = 0;
+empCheck = Math.floor(Math.random()*10)%3;
+
+switch(empCheck){
+    case IS_FULL_TIME: 
+            empHours = PART_TIME_HOURS;
+            break;
+
+    case IS_FULL_TIME: 
+            empHours = FULL_TIME_HOURS;
+            break;
+    default:
+            empHours = 0;
 }
-else {
-    console.log("Employee is Present");
-}
+
+let empWage = empHours * WAGE_PER_HOUR;
+console.log("Employee Wage : "+empWage);
